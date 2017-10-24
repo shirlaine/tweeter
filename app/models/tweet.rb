c5c58b520d675f10483d754285d1817f9f89a02a
shirlaine <shirlaine.phang@gmail.com>
2017-10-24 22:35:18 +0800

    Add validations to tweet model and model specs

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+
+  validates :body,
+            presence: true,
+            length: { maximum: 140 }
+
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { expect(subject).to validate_presence_of(:body) }
+    it { expect(subject).to validate_length_of(:body). is_at_most(140) }
+  end
 end
