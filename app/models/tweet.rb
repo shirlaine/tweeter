1fae148b20252796dc243e22cd8f1f00e33eaa32
shirlaine <shirlaine.phang@gmail.com>
2017-10-25 21:47:38 +0800

    Add validations and associations for user model and associations for tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
 
+  belongs_to :user
+
   validates :body,
             presence: true,
             length: { maximum: 140 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
 
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]{3,15}\z/, message: 'only allows letters and numbers' }
+  validates :fullname, presence: true, length: { in: 3..20 }
+
+  has_many :tweets, dependent: :destroy
+
 end
