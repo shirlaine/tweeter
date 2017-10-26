86155c637b7961bd84d3176aae155ca91ef93fd9
shirlaine <shirlaine.phang@gmail.com>
2017-10-26 21:21:01 +0800

    Add model and migraton for TweetTag join table, add associations with Tweet and Tag

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ApplicationRecord
 
   belongs_to :user
+  has_many :tweet_tags
+  has_many :tags, through: :tweet_tags
 
   validates :body,
             presence: true,
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025133432) do
+ActiveRecord::Schema.define(version: 20171026125614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", force: :cascade do |t|
+    t.bigint "tweet_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_tweet_tags_on_tag_id"
+    t.index ["tweet_id"], name: "index_tweet_tags_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "body", null: false
@@ -36,5 +51,7 @@ ActiveRecord::Schema.define(version: 20171025133432) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweet_tags", "tags"
+  add_foreign_key "tweet_tags", "tweets"
   add_foreign_key "tweets", "users"
 end
