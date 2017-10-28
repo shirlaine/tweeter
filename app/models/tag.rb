446491bdb081bad7384a6a3ca0ef2fc963d73a0a
shirlaine <shirlaine.phang@gmail.com>
2017-10-28 18:43:25 +0800

    Re-add validations for name and to allow tweet new to save empty tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ApplicationRecord
   has_many :tweet_tags, dependent: :destroy
   has_many :tweets, through: :tweet_tags
 
-  # validates :name, presence: true, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 15 }
 
 end
