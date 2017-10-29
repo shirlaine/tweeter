c6804b8bf3084adaea9a9563d183ed0dc76e7ff5
shirlaine <shirlaine.phang@gmail.com>
2017-10-29 21:50:58 +0800

    Add validations to tweettag model, rspec tests for relationship and tweetag models

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { expect(subject).to validate_presence_of(:leader) }
+  it { expect(subject).to validate_presence_of(:follower) }
+
 end
