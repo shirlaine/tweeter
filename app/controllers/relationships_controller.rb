aacae666e2d62507481ab46281e7bec30033a559
shirlaine <shirlaine.phang@gmail.com>
2017-10-29 21:44:06 +0800

    Add flash messages to follow and unfollow and edit unfoollow button style

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,12 +3,14 @@ class RelationshipsController < BaseController
   def create
     @user = User.find(params[:follower_id])
     @relationship = current_user.follow!(@user)
+    flash[:alert] = "You are now following @#{@user.username}"
     redirect_to user_path(@user)
   end
 
   def destroy
     @user = User.find(params[:id])
     current_user.unfollow(@user)
+    flash[:alert] = "You have unfollowed @#{@user.username}"
     redirect_to user_path(@user)
   end
 
