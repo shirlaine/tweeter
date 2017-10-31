41b9b6590e86d0a7ed0866d4a30c0a42093c1bb4
shirlaine <shirlaine.phang@gmail.com>
2017-10-31 19:50:26 +0800

    Add dependent destroy to tweet and tag model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
   has_many :tweet_tags, dependent: :destroy
-  has_many :tags, through: :tweet_tags
+  has_many :tags, through: :tweet_tags, dependent: :destroy
   accepts_nested_attributes_for :tags, reject_if: ->(attributes) { attributes['name'].blank? }
   validates :body,
             presence: true,
