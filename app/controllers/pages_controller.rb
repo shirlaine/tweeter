bef15978b257f715c40b3428ec578dee53776832
shirlaine <shirlaine.phang@gmail.com>
2017-10-31 22:04:03 +0800

    Add create tweet in index actions and views. edit styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < BaseController
   def index
     @user = current_user
     @tweets = current_user.tweets.order(id: :desc)
-    @leaders= current_user.leaders
+    @leaders = current_user.leaders
+    @tweet = Tweet.new
+    @tag = @tweet.tags.new
   end
 
 end
