a29995af889f6b4dd5201f4c6690279e62eefd26
shirlaine <shirlaine.phang@gmail.com>
2017-11-03 11:33:50 +0800

    Add tweettag model specs

--- spec/models/tweet_tag_spec.rb
+++ spec/models/tweet_tag_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe TweetTag, type: :model do
   it { expect(subject).to validate_presence_of(:tweet) }
   it { expect(subject).to validate_presence_of(:tag) }
 
+  it { expect(subject).to belong_to(:tweet) }
+  it { expect(subject).to belong_to(:tag) }
+
 end
