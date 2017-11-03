9a20d6f70dd17e104cc60d74dc0fa5002a381a9d
shirlaine <shirlaine.phang@gmail.com>
2017-11-03 11:26:10 +0800

    Add tag model specs

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Tag, type: :model do
   let(:subject) { create(:tag) }
 
   describe 'validations' do
-    # it { expect(subject).to validate_presence_of(:name) }
-    # it { expect(subject).to validate_length_of(:name).is_at_most(15) }
+    it { expect(subject).to validate_presence_of(:name) }
+    it { expect(subject).to validate_length_of(:name). is_at_most(15) }
+    it { expect(subject).to have_many(:tweets) }
+    it { expect(subject).to have_many(:tweet_tags) }
   end
 end
