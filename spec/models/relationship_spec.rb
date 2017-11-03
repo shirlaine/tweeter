092255bbb1aa42e68b4b768cbf4ffa0f8838bb07
shirlaine <shirlaine.phang@gmail.com>
2017-11-03 11:32:32 +0800

    Add relationship model specs

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Relationship, type: :model do
   it { expect(subject).to validate_presence_of(:leader) }
   it { expect(subject).to validate_presence_of(:follower) }
 
+  it { expect(subject).to belong_to(:leader) }
+  it { expect(subject).to belong_to(:follower) }
+
 end
